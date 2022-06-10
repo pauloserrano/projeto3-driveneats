9e54c96f22f745aa442e532bc6052a3681dd1053
Paulo Serrano <pvsserrano@gmail.com>
2022-06-10 01:15:52 -0300

    item selection and submit

--- script/main.js
+++ script/main.js
@@ -1,17 +1,37 @@
-const optionItems = document.querySelectorAll('.options > div')
+const optionItems = document.querySelectorAll('.options')
+const submitBtn = document.querySelector('.btn-submit')
+let selectedItems = 0
 
+const removePreviousItem = (container) => {
+    const containerItems = container.querySelectorAll('div')
+    console.log(containerItems)
 
-const removePreviousItem = () => {
-    optionItems.forEach(item => {
+    containerItems.forEach(item => {
         if (item.classList.contains('selected')){
             item.classList.remove('selected')
+            selectedItems -= 1
         }
     })
+
 }
 
-optionItems.forEach(item => {
-    item.addEventListener('click', () => {  
-        removePreviousItem()
-        item.classList.add('selected')
+optionItems.forEach(container => {
+    const items = container.querySelectorAll('div')
+
+    items.forEach(item => {
+        item.addEventListener('click', (e) => {  
+            const isSelected = item.classList.contains('selected')
+
+            if (!isSelected){
+                removePreviousItem(e.target.parentElement)
+                item.classList.add('selected')
+                selectedItems += 1
+            }
+    
+            if (selectedItems === 3) {
+                submitBtn.classList.add('ready')
+            }
+        })
     })
 })
+
